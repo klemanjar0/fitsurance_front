0c0e3bd551f45689915cb8839088aea4e49d9338
klemanjar0 <klim2838@gmail.com>
2021-03-31 10:21:36 +0300

    v2021.03.31.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,13 +8,12 @@ export default function configureStore(initialState) {
     ];
     const store = createStore(rootReducer, initialState, compose(
         applyMiddleware(...middlewares),
-        window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools
+        window.devToolsExtension ? window.devToolsExtension() : f => f
         )
     );
     if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
         module.hot.accept('../reducers', () => {
-            const nextReducer = require('../reducers').default; // eslint-disable-line global-require
+            const nextReducer = require('../reducers').default;
             store.replaceReducer(nextReducer);
         });
     }
